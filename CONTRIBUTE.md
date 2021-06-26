056670ef63a1e02cbc127a67f27c8d9442951ebe
Julien Delange <julien@gunnm.org>
2021-06-25 18:43:17 -0600

    Handle parameters for javascript and typescript

---
+++ CONTRIBUTE.md
@@ -0,0 +1,12 @@
+# Contribute
+
+## Report issues
+
+Please use the [GitHub issues](https://github.com/codeinspectorio/vscode-plugin/issues)
+to report issues directly.
+
+## Publish a new releae
+
+We use GitHub release to publish a new version of the plugin
+Make sure you increase the version number in `package.json`
+before triggering a new relase.
--- src/graphql-api/mutations.ts
+++ src/graphql-api/mutations.ts
@@ -2,16 +2,18 @@ import { gql } from "graphql-request";
 
 export const CREATE_FILE_ANALYSIS: string = gql`
   mutation createFileAnalysis(
-    $language: LanguageEnumeration!,
-    $code: String!,
-    $filename: String!,
+    $language: LanguageEnumeration!
+    $code: String!
+    $filename: String!
     $projectId: Long
+    $parameters: String
   ) {
     createFileAnalysis(
-      language: $language,
-      code: $code,
-      filename: $filename,
+      language: $language
+      code: $code
+      filename: $filename
       projectId: $projectId
+      parameters: $parameters
     )
     recordAccess(accessType: VsCode, actionType: FileAnalysisRequest)
   }
