af842e71c6c0358b069a961ecfeebe5ed23fc155
Julien Delange <julien@gunnm.org>
2022-07-27 16:03:34 -0500

    attempt

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -31,7 +31,6 @@ async function main() {
       await runTests({
         extensionDevelopmentPath,
         extensionTestsPath,
-        version: "1.69.0",
         launchArgs: [
           // This disables all extensions except the one being tested
           "--disable-extensions",
